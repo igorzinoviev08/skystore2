e2b2fd7aa261fb07a67c28b4dfdc0a4cee6f7799
igorzinoviev08 <zinovievigor08@gmail.com>
2023-09-22 03:36:05 +0300

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.metod == 'POST':
+        visiter = {}
+        visiter['name'] = request.POST.get('name', None)
+        visiter['phone'] = request.POST.get('phone', None)
+        visiter['massage'] = request.POST.get('message', None)
+        print(visiter)
+    return render(request, 'catalog/contacts.html')
