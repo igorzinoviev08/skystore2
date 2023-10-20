6c1ea850a3b8ecc337541d536fb9737f52fb7cfa
igorzinoviev08 <zinovievigor08@gmail.com>
2023-10-20 14:33:42 +0300

    HW_21.1 blog

--- catalog/views.py
+++ catalog/views.py
@@ -1,70 +1,10 @@
 from django.conf import settings
-from django.views.generic import ListView
+from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 import blog
-
-
-class BlogListView(ListView):
-    model = blog
-
-    def get_queryset(self, *args, **kwargs):
-        queryset = super().get_queryset(*args, **kwargs)
-        queryset = queryset.filter(is_published=True)
-        return queryset
-
-class BlogCreateView(CreateView):
-    model = blog
-    fields = ('name', 'description', 'image')
-    success_url = reverse_lasy('blog:list')
-
-class BlogUpdateView(UpdateView):
-    model = blog
-    fields = ('name', 'description', 'image')
-    success_url = reverse_lasy('blog:list')
-
-class BlogDeleteView(DeleteView):
-    model = blog
-    #fields = ('name', 'description', 'image')
-    success_url = reverse_lasy('blog:list')
-
-class BlogDetailView(DetailView):
-    model = blog
-
-    def get_object(self, queryset=None):
-        self.object = super().get_object(queryset)
-        self.object.view_count +=1
-        if self.object.view_count == 100
-            send_mail(
-                subject='ПОЗДРАВЛЕНИЕ',
-                message=f'Поздравляем ваш пост {self.object.name} набрал 100 просмотров',
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=['igor.zinoviev08@yandex.ru'],
-                fail_silently=False,
-            )
-
-        self.object.save()
-        return self.object
-
-
-
-
-
-
-
-
-
-#from catalog.models import Product, Contact
-
-
-
-
-
-
-
-
-
-
-
+from catalog.models import Product, Contact
 
 
 class ProductListView(ListView):
--- skystore/settings.py
+++ skystore/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
-    'blog',
+    'blog'
+
 ]
 
 
