d70f6c7cf610013ac490db293340aceeca9b64d6
igorzinoviev08 <zinovievigor08@gmail.com>
2023-10-20 14:39:12 +0300

    HW_21.1 blog

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from pytils.translit import slugify
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100, verbose_name='заголовок')
+    slug = models.SlugField(verbose_name='slug', unique=True)
+    description =models.TextField(verbose_name='содержимое')
+    image = models.ImageField(upload_to='blog/', verbose_name='изображение')
+    issued_date = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='признак публикации')
+    views_count = models.PositiveIntegerField(default=0, verbose_name='количество просмотров')
+
+
+    def __str__(self):
+        return  f'{self.name}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            super().save(*args, **kwargs)
+
+
+
+
---
+++ blog/views.py
@@ -0,0 +1,56 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView, DetailView
+
+import blog
+from blog.models import Blog
+
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
+
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('name', 'description', 'image')
+    success_url = reverse_lazy('blog:list')
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('name', 'description', 'image')
+    success_url = reverse_lazy('blog:list')
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    #fields = ('name', 'description', 'image')
+    success_url = reverse_lazy('blog:list')
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count +=1
+        if self.object.view_count == 100:
+            send_mail(
+                subject='ПОЗДРАВЛЕНИЕ',
+                message=f'Поздравляем ваш пост {self.object.name} набрал 100 просмотров',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=['igor.zinoviev08@yandex.ru'],
+                fail_silently=False,
+            )
+
+        self.object.save()
+        return self.object
+
