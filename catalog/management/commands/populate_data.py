2fded13650ac78e425890187843a9b6f58e2cc23
igorzinoviev08 <zinovievigor08@gmail.com>
2023-10-30 23:35:28 +0300

    доработано

--- catalog/management/commands/populate_data.py
+++ catalog/management/commands/populate_data.py
@@ -1,47 +1,49 @@
-import  json
+import json
 
 from django.core.management import BaseCommand
 
-from catalog.models import Product, Catrgory, Contact
-from skystore.settings import  BASE_DIR
+from catalog.models import Product, Contact, Category
+from skystore.settings import BASE_DIR
+
+
 class Command(BaseCommand):
     help = 'Заполнение базы данных фикстурами'
 
     def handle(self, *args, **options):
-        Product.object.all().delete()
-        Category.object.all().delete()
-        Contact.object.all().delete()
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+        Contact.objects.all().delete()
 
-        with open(BASE_DIR/'catalog/fixtures/category.json', encoding='utf-8') as fp:
+        with open(BASE_DIR / 'catalog/fixtures/categories.json', encoding='utf-8') as fp:
             category_data = json.load(fp)
             for item in category_data:
-                Catrgory.objects.create(
-                    pk= item['pk'],
-                    name = item["fields"]["name"],
-                    description = item["fields"]["description"]
+                Category.objects.create(
+                    pk=item['pk'],
+                    name=item["fields"]["name"],
+                    description=item["fields"]["description"]
                 )
-        with open(BASE_DIR/'catalog/fixtures/product.json', encoding='utf-8') as fp:
+        with open(BASE_DIR / 'catalog/fixtures/products.json', encoding='utf-8') as fp:
             product_data = json.load(fp)
             for item in product_data:
                 category_pk = item["fields"]["category"]
                 category = Category.objects.get(pk=category_pk)
                 Product.objects.create(
-                    pk= item['pk'],
+                    pk=item['pk'],
                     name=item["fields"]["name"],
                     description=item["fields"]["description"],
                     preview_description=item["fields"]["preview_description"],
-                    image=item["fields"]["image"]
+                    image=item["fields"]["image"],
                     category=category,
                     price=item["fields"]["price"],
                     created_at=item["fields"]["created_at"],
                     last_modified=item["fields"]["last_modified"],
                 )
 
-        with open(BASE_DIR/'catalog/fixtures/contact.json', encoding='utf-8') as fp:
+        with open(BASE_DIR / 'catalog/fixtures/contacts.json', encoding='utf-8') as fp:
             contact_data = json.load(fp)
             for item in contact_data:
                 Contact.objects.create(
-                    pk= item['pk'],
+                    pk=item['pk'],
                     country=item["fields"]["country"],
                     inn=item["fields"]["inn"],
-                    address=item["fields"]["address"] )
+                    address=item["fields"]["address"])
